b924fd06f310bed12dda15605be8710ba296fabf
Zois Pagoulatos <zpagoulatos@hotmail.com>
2018-12-12 12:40:28 +0100

    Add snow

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div id="app" class="px-2 py-12">
+    <Snowf
+      v-if="showSnow"
+      :amount="50"
+      :size="5"
+      :speed="1.5"
+      :wind="0"
+      :opacity="0.8"
+      :swing="1"
+      :image="null"
+      :zIndex="null"
+      :resize="true"
+      color="#fff"
+    />
     <NrgLogo />
     <NrgPlayer />
     <NrgSongInfo />
@@ -10,6 +23,7 @@
 </template>
 
 <script>
+import Snowf from "vue-snowf";
 import NrgLogo from "./components/NrgLogo.vue";
 import NrgPlayer from "./components/NrgPlayer.vue";
 import NrgSongInfo from "./components/NrgSongInfo.vue";
@@ -20,12 +34,18 @@ import NrgFooter from "./components/NrgFooter.vue";
 export default {
   name: "Nrg",
   components: {
+    Snowf,
     NrgLogo,
     NrgPlayer,
     NrgSongInfo,
     NrgSocialFacebook,
     NrgSocialTwitter,
     NrgFooter
+  },
+  methods: {
+    showSnow() {
+      return new Date().getMonth() === 11;
+    }
   }
 };
 </script>
