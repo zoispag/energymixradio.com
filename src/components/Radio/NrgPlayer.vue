54e8121c41a9d1689f3983e0dd101e1fade515a9
Zois Pagoulatos <zpagoulatos@hotmail.com>
2022-07-17 14:29:41 +0300

    Vue 3.x + Vite + Tailwind 3.x + Note 18.x Rewrite

---
+++ src/components/Radio/NrgPlayer.vue
@@ -0,0 +1,12 @@
+<script setup>
+const playerSource = "https://cast.magicstreams.gr/sc/jziwpfyo/stream";
+</script>
+
+<template>
+  <div class="p-4">
+    <audio controls autoplay>
+      <source :src="playerSource" type="audio/mp3" />
+      Your browser does not support the audio element.
+    </audio>
+  </div>
+</template>
